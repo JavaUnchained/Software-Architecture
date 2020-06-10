4522d5bf2709aad268e76c78c14ab5e927a1a2f8
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-11 02:59:40 +0300

    add adress params

--- foodeliver/src/main/java/com/example/foodeliver/entity/Adress.java
+++ foodeliver/src/main/java/com/example/foodeliver/entity/Adress.java
@@ -1,4 +1,18 @@
 package com.example.foodeliver.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ADRESS")
+@Data
 public class Adress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private String street;
+    private String flat;
 }
