0203ec90bfd8f9904aef7b66e9ccd5b33af25935
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-11 00:59:59 +0300

    Add src

---
+++ foodeliver/src/main/java/com/example/foodeliver/FoodeliverApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodeliverApplication.class, args);
+    }
+
+}
---
+++ foodeliver/src/test/java/com/example/foodeliver/FoodeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodeliverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
