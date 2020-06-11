4afe62b43f482412e580b13a0bedd94a1bbd5ed8
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-12 00:28:02 +0300

    some changed

--- foodeliver/src/main/java/com/example/foodeliver/controller/OperatorController.java
+++ foodeliver/src/main/java/com/example/foodeliver/controller/OperatorController.java
@@ -1,9 +1,14 @@
 package com.example.foodeliver.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OperatorController {
+    @GetMapping("/operatorpage")
+    public String operatorpage() {
+        return "operatorpage";
+    }
 
 
 }
