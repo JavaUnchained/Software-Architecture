9fe9c8da6c8cb595797faa1368d0a118a54107e8
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-12 01:05:05 +0300

    some changed

--- foodeliver/src/main/java/com/example/foodeliver/controller/OperatorController.java
+++ foodeliver/src/main/java/com/example/foodeliver/controller/OperatorController.java
@@ -1,14 +1,25 @@
 package com.example.foodeliver.controller;
 
+import com.example.foodeliver.repository.RationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OperatorController {
+    @Autowired
+    private RationRepository rationRepository;
+
     @GetMapping("/operatorpage")
-    public String operatorpage() {
+    public String operatorPage() {
         return "operatorpage";
     }
 
+    @GetMapping("/rations")
+    public String rationsPage(Model model) {
+        model.addAttribute("rations", rationRepository.findAll());
+        return "rations";
+    }
 
 }
