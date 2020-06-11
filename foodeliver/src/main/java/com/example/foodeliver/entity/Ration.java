b803424c06a40f4b61366bc99e4e85d4aaac5379
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-11 23:08:44 +0300

    complite Ration

--- foodeliver/src/main/java/com/example/foodeliver/entity/Ration.java
+++ foodeliver/src/main/java/com/example/foodeliver/entity/Ration.java
@@ -1,4 +1,22 @@
 package com.example.foodeliver.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Ration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rationName;
+    private String rationDescription;
+    private String price;
 }
