7f4d09d7562e947af30473b0e999571f0f5f0487
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-13 02:27:06 +0300

    sch

--- foodeliver/src/main/java/com/example/foodeliver/entity/users/Client.java
+++ foodeliver/src/main/java/com/example/foodeliver/entity/users/Client.java
@@ -1,11 +1,11 @@
 package com.example.foodeliver.entity.users;
 
+import com.example.foodeliver.entity.Account;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "client")
@@ -14,8 +14,12 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class Client extends User {
 
-    public Client(String surname, String name, String phoneNumber, String username, String password, Role roleId) {
+    public Client(String surname, String name, String phoneNumber, String username, String password, Role roleId, Account account) {
         super(surname, name, phoneNumber, username, password, roleId);
+        this.account = account;
     }
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id")
+    Account account;
 }
--- foodeliver/src/main/java/com/example/foodeliver/entity/users/Operator.java
+++ foodeliver/src/main/java/com/example/foodeliver/entity/users/Operator.java
@@ -1,17 +1,23 @@
 package com.example.foodeliver.entity.users;
 
+import com.example.foodeliver.entity.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "operator")
 @Data
 @NoArgsConstructor
 public class Operator extends User {
-    public Operator(String surname, String name, String phoneNumber, String username, String password, Role roleId) {
+
+    public Operator(String surname, String name, String phoneNumber, String username, String password, Role roleId, Account account) {
         super(surname, name, phoneNumber, username, password, roleId);
+        this.account = account;
     }
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id")
+    Account account;
 }
