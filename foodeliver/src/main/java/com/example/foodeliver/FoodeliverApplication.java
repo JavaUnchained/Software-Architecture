543f1956eb055cb693fa92793db0b7d1cef69a76
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-13 01:22:43 +0300

    sch

--- foodeliver/src/main/java/com/example/foodeliver/FoodeliverApplication.java
+++ foodeliver/src/main/java/com/example/foodeliver/FoodeliverApplication.java
@@ -1,19 +1,15 @@
 package com.example.foodeliver;
 
 import com.example.foodeliver.entity.users.Courier;
-import com.example.foodeliver.entity.users.Operator;
-import com.example.foodeliver.entity.users.Client;
 import com.example.foodeliver.entity.users.Role;
+import com.example.foodeliver.repository.ClientRepository;
 import com.example.foodeliver.repository.CourierRepository;
 import com.example.foodeliver.repository.OperatorRepository;
-import com.example.foodeliver.repository.ClientRepository;
 import com.example.foodeliver.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FoodeliverApplication implements CommandLineRunner{
@@ -34,24 +30,24 @@ public class FoodeliverApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        Role roleCourier = new Role("ROLE_COURIER");
-        roleRepository.save(roleCourier);
-        Role roleOperator = new Role("ROLE_OPERATOR");
-        roleRepository.save(roleOperator);
-        Role roleClient = new Role("ROLE_CLIENT");
-        roleRepository.save(roleClient);
-        Role roleCook = new Role("ROLE_COOK");
-        roleRepository.save(roleCook);
-
-        Courier courier1 =new Courier();
-        courier1.setSurname("CourierIvanov");
-        courier1.setName("CourierIvan");
-        courier1.setPhoneNumber("8-910-111-11-11");
-        courier1.setUsername("Courier");
-        courier1.setPassword("$2a$10$xDFKjI/oB3/d95NzD7e.Xebe2PzO.2y4Ilbtw34PoOj1jF/ZhHQOi");
-        courier1.setRoleId(roleCourier);
-
-        courierRepository.save(courier1);
+//        Role roleCourier = new Role("ROLE_COURIER");
+//        roleRepository.save(roleCourier);
+//        Role roleOperator = new Role("ROLE_OPERATOR");
+//        roleRepository.save(roleOperator);
+//        Role roleClient = new Role("ROLE_CLIENT");
+//        roleRepository.save(roleClient);
+//        Role roleCook = new Role("ROLE_COOK");
+//        roleRepository.save(roleCook);
+//
+//        Courier courier1 =new Courier();
+//        courier1.setSurname("CourierIvanov");
+//        courier1.setName("CourierIvan");
+//        courier1.setPhoneNumber("8-910-111-11-11");
+//        courier1.setUsername("Courier");
+//        courier1.setPassword("$2a$10$xDFKjI/oB3/d95NzD7e.Xebe2PzO.2y4Ilbtw34PoOj1jF/ZhHQOi");
+//        courier1.setRoleId(roleCourier);
+//
+//        courierRepository.save(courier1);
 
     }
 
--- foodeliver/src/main/java/com/example/foodeliver/entity/users/Courier.java
+++ foodeliver/src/main/java/com/example/foodeliver/entity/users/Courier.java
@@ -1,19 +1,13 @@
 package com.example.foodeliver.entity.users;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "courier")
-@Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Courier extends User {
 
     public Courier(String surname, String name, String phoneNumber,
