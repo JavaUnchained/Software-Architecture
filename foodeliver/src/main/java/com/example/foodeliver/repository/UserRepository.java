89f9189509d8c63a0a08bf0e937ca9217e8f4a78
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-12 12:35:22 +0300

    sch

--- foodeliver/src/main/java/com/example/foodeliver/repository/UserRepository.java
+++ foodeliver/src/main/java/com/example/foodeliver/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.foodeliver.repository;
 
-public interface UserRepository {
+import com.example.foodeliver.entity.person.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByName();
 }
