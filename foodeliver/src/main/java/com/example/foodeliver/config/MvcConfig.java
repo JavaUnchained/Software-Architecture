8066093b61e7508c8b85d1e3a17a0fcc2db613db
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-12 12:50:38 +0300

    sch

--- foodeliver/src/main/java/com/example/foodeliver/config/MvcConfig.java
+++ foodeliver/src/main/java/com/example/foodeliver/config/MvcConfig.java
@@ -1,4 +1,12 @@
 package com.example.foodeliver.config;
 
-public class MvcConfig {
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
+    }
 }
