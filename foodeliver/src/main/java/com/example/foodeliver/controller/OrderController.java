cdad87cd4a15e36a9b5fff9bfe42518f82e1970c
PC <AndreyVladimirov1ch@yandex.ru>
2020-06-13 05:35:11 +0300

    sch

--- foodeliver/src/main/java/com/example/foodeliver/controller/OrderController.java
+++ foodeliver/src/main/java/com/example/foodeliver/controller/OrderController.java
@@ -81,15 +81,28 @@ public class OrderController {
         return "client_order";
     }
 
+    @GetMapping("/operator_order")
+    public String operatorOrder(Model model) {
+        model.addAttribute("orders", orderService.getAllOrders());
+        return "operator_order";
+    }
+
+//    @PostMapping("/operator_order")
+//    public String rationsSumbit(@RequestParam String name,
+//                                @RequestParam Double price,
+//                                @RequestParam String description,
+//                                Model model) {
+//        Ration ration = rationService.rationFactoryMethod(name,price,description);
+//        rationService.saveRation(ration);
+//
+//        model.addAttribute("rations", rationService.getAllRations());
+//        return "operator_order";
+//    }
+
     public Client getCurrentClient() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentPrincipalName = authentication.getName();
         return clientService.getClientByUsername(currentPrincipalName);
     }
 
-    public Operator getCurrentOperator() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentPrincipalName = authentication.getName();
-        return operatorService.getOperatorByUsername(currentPrincipalName);
-    }
 }
--- foodeliver/src/main/java/com/example/foodeliver/repository/OrderRepository.java
+++ foodeliver/src/main/java/com/example/foodeliver/repository/OrderRepository.java
@@ -3,5 +3,6 @@ package com.example.foodeliver.repository;
 import com.example.foodeliver.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {}
